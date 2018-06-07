afa763b8fcc103d7dc22d57fb69a36f946d60151
C_Kumar, Kundan <C_Kumar.Kundan@cerner.com>
2018-06-07 15:38:12 +0530

    selenium js

---
+++ app.js
@@ -0,0 +1,19 @@
+const webdriver = require('selenium-webdriver'),
+   By = webdriver.By,
+   until = webdriver.until;
+const chrome = require("selenium-webdriver/chrome");
+const chromeCapabilities = webdriver.Capabilities.chrome();
+const chromeOptions = {
+   'args': ['--load-extension=C:/Accessibiity/LatestAccessibility/Extension/a11yChromeExtn']
+}; //set the path of extension
+
+chromeCapabilities.set('chromeOptions', chromeOptions);
+//const a11yUrlStart = 'https://localhost:3005/api/registerRunDetail?teamName=MPagesAT3&component=PatientInformationTest1&trigger=CIT&run=start';
+
+const driver = new webdriver.Builder()
+.withCapabilities(chromeCapabilities)
+.forBrowser("chrome")
+.build();
+driver.get("http://localhost:4200/");
+//driver.wait(1000);
+//driver.quit();
