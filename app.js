d966ebf63b256bc88318c3ccbc3dd9a36cf144aa
C_Kumar, Kundan <C_Kumar.Kundan@cerner.com>
2018-09-10 18:33:17 +0530

    added

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const webdriver = require('selenium-webdriver'),
    By = webdriver.By,
    until = webdriver.until;
 const chrome = require("selenium-webdriver/chrome");
+var getenv = require('getenv');
 const chromeCapabilities = webdriver.Capabilities.chrome();
 const chromeOptions = {
-   'args': ['--load-extension=C:/A11YExtensionTarget']
+   'args': ['--load-extension=C:/KeshriKundanAccess/extension/a11yChromeExtn']
 }; //set the path of extension
-
+var host = getenv('ACCESSIBILITY');
+console.log(host);
 chromeCapabilities.set('chromeOptions', chromeOptions);
-//const a11yUrlStart = 'https://localhost:3005/api/registerRunDetail?teamName=MPagesAT3&component=PatientInformationTest1&trigger=CIT&run=start';
-
 const driver = new webdriver.Builder()
 .withCapabilities(chromeCapabilities)
 .forBrowser("chrome")
 .build();
-driver.get("http://localhost:4200/");
+driver.get("http://localhost:3000/");
 //driver.wait(1000);
 //driver.quit();
